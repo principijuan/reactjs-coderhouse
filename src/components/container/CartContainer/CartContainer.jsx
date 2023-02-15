3cb74a7e6b2464820a54e91ae4605a36f4e8c400
Juan Principi <principijuan@gmail.com>
2023-02-14 22:59:57 -0300

    final commit

--- src/components/container/CartContainer/CartContainer.jsx
+++ src/components/container/CartContainer/CartContainer.jsx
@@ -1,12 +1,52 @@
 import { Link } from "react-router-dom"
 import { useCartContext } from "../../../context/CartContext"
 import { CartWidget } from "../../CartWidget/CartWidget"
+import { addDoc, collection, getFirestore } from "firebase/firestore"
 import "./CartContainer.css"
+import { useState } from "react"
 
 const CartContainer = () => {
+  const [dataForm, setDataForm] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    validarEmail: ""
+
+  })
   
   const { cartList, vaciarCarrito, precioTotal, eliminarItem } = useCartContext()
   
+  const generarOrden = (evento) => {
+    evento.preventDefault()
+    
+    const order = {}
+
+    order.buyer = dataForm
+
+    order.items = cartList.map(({id, nombre, precio}) => ({nombre, precio, id}))
+
+    order.total = precioTotal()
+
+    console.log(order)
+
+    const db = getFirestore()
+
+    const queryCollection = collection(db, "orders")
+
+    addDoc(queryCollection, order)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+  }
+
+
+  const handleOnChange = (evt) => {
+    setDataForm({
+      ...dataForm,
+      [evt.target.name]: evt.target.value
+    })
+  }
+  console.log(dataForm)
+
   return (
     <div className="cartContainer">
       {cartList.map ( prod => 
@@ -19,22 +59,68 @@ const CartContainer = () => {
             <label>Precio: <b>${prod.precio}</b></label><br />
           </div>
         </ div> )}
-        <div className="resumenCarrito">
-          {precioTotal() != 0 && <h4>Precio total: ${precioTotal()}</h4>}
-          {precioTotal() != 0 && <h4 className="resumenCantidad">Cantidad de productos: <CartWidget /></h4>}
-          {precioTotal() != 0 && <button onClick={vaciarCarrito} className="btn btn-outline-danger">Vaciar carrito</button>}
 
-          {precioTotal() === 0 &&
-            <div className="carritoVacio">
-              <h2>No hay nada en el carrito</h2>
-              <div>              
-                <Link to={"/"}>
-                <button className="btn btn-outline-primary">Volver al inicio</button>
+          <div>
+            <div className="resumenCarrito">
+              {precioTotal() != 0 && 
+                <div> 
+                  <form onSubmit={generarOrden} className="formularioCarrito">
 
-                </Link>
+                    <div className="formulariosInput">
+                    <h3>Complete sus datos</h3>
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Ingrese su nombre"
+                        value={dataForm.name}
+                        onChange={handleOnChange}/>
+
+                      <input
+                        type="text"
+                        name="phone"
+                        placeholder="Ingrese su teléfono"
+                        value={dataForm.phone}
+                        onChange={handleOnChange}/>
+
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Ingrese su Email"
+                        value={dataForm.email}
+                        onChange={handleOnChange}/>
+
+                      <input
+                        type="text"
+                        name="validarEmail"
+                        placeholder="Confirme su Email"
+                        value={dataForm.validarEmail}
+                        onChange={handleOnChange}/>
+                    </div>
+                    <div className="detalleCarrito">
+                      <h4>Precio total: ${precioTotal()}</h4>
+                      <h4 className="resumenCantidad">Cantidad de productos: <CartWidget /></h4>
+                      <button onClick={generarOrden} className="btn btn-outline-primary">Generar orden</button>
+                      <button onClick={vaciarCarrito} className="btn btn-outline-danger">Vaciar carrito</button>
+                    </div>
+                    
+                  </form>
+                </ div>}
+
+              {precioTotal() === 0 &&
+                <div className="carritoVacio">
+                  <h2>No hay nada en el carrito</h2>
+                  <div>              
+                    <Link to={"/"}>
+                      <button className="btn btn-outline-primary">Volver al inicio</button>
+                    </Link>
+                  </div>
+                </ div>}
               </div>
-            </ div>}
-        </div>
+
+
+            </div>
+
+
     </div>
   )
 }
